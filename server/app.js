186c8d8b100aaa84b3544d259b94c898e65f7313
ecorreia <design@elsoncorreia.com>
2019-02-09 15:44:32 -0500

    jade and static file serve

--- server/app.js
+++ server/app.js
@@ -1,4 +1,14 @@
+import path from 'path';
 import express from 'express';
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(3000, () => {
-  console.log('server: running at port 3000');
+const PORT = process.env.PORT || 4500;
+
+app.listen(PORT, () => {
+  console.log(`server: running at port ${PORT}`);
 });
